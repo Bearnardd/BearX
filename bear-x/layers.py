28fa732195ff31a46b4b72049b57156070e4f649
bartekkz <bartek.szmelczynski@gmail.com>
2019-12-26 15:50:30 +0100

    init library directory

---
+++ bear-x/layers.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+class Layer:
+    def __init__(self, **kwargs):
+        allowed_kwargs = {
+            ""
+        }
+        for kwarg in kwargs:
+            if kwargs ni all
