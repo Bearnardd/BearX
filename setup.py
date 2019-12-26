8b4abefacf04c1f74face443742165627cd817ab
bartekkz <bartek.szmelczynski@gmail.com>
2019-12-26 15:32:16 +0100

    create setup file for library

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
-    name="DeepX",
+setup(
+    name="bearx",
     version="0.1",
-    scripts=['deepx'],
+    scripts=['bearx'],
     author="Bartek Szmelczynski",
     author_email="bartek.szmelczynski@gmail.com",
     description=("deep learning library created in order to get in ",
                  "depth knownledge about neural nets"),
     long_description=long_description,
-    url="https://github.com/bartekkz/DeepX",
-    packages=setuptools.find_packages(),
+    url="https://github.com/bartekkz/BearX",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
 )
