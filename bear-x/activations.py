9cbbf91ace1877cb92f81cf880f8095ce2e681c7
Bartekkz <bartosz.szmelczynski@gmail.com>
2019-12-27 02:28:25 +0100

    convert actiovations function back to regular separate functions

--- bear-x/activations.py
+++ bear-x/activations.py
@@ -8,36 +8,20 @@ from tensor import Tensor
 import numpy as np
 
 
-class Activation:
-    def calc(self, inputs: Tensor) -> Tensor:
-        raise NotImplementedError(
-            "Not implementeed in base class!"
-        )
 
-class relu(Activation):
-    def __getitem__(self):
-        return "relu"
+def relu(inputs: Tensor) -> Tensor:
+    assert type(inputs) == np.ndarray
+    outputs = inputs.copy()
+    for node in range(len(outputs)):
+        if outputs[node] > 0:
+            continue
+        else:
+            outputs[node] = 0
+    return outputs 
  
-    @staticmethod
-    def calc(inputs: Tensor) -> Tensor:
-        assert type(inputs) == np.ndarray
-        outputs = inputs.copy()
-        for node in range(len(outputs)):
-            if outputs[node] > 0:
-                continue
-            else:
-                outputs[node] = 0
-        return outputs 
-
-class sigmoid(Activation):
-    def __getitem__(self):
-        return "sigmoid"
-
-    
-    @staticmethod
-    def calc(inputs: Tensor) -> Tensor:
-        assert type(inputs) == np.ndarray
-        outputs = inputs.copy()
-        for node in range(len(outputs)):
-            outputs[node] = 1 / (1 + np.exp(-outputs[node]))
-        return outputs
+def sigmoid(inputs: Tensor) -> Tensor:
+    assert type(inputs) == np.ndarray
+    outputs = inputs.copy()
+    for node in range(len(outputs)):
+        outputs[node] = 1 / (1 + np.exp(-outputs[node]))
+    return outputs
