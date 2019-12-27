3d0606ea39c405ec9c49501e8a5ea7f85d6d843a
Bartekkz <bartosz.szmelczynski@gmail.com>
2019-12-27 16:46:56 +0100

    add MSE

---
+++ bear-x/losses.py
@@ -0,0 +1,29 @@
+from tensor import Tensor
+
+import numpy as np
+
+
+class Loss:
+    def loss(self, predicted: Tensor, target: Tensor) -> float:
+        raise NotImplementedError(
+            "Not implemented in base class!"
+        )
+
+    def gradient(self, pred: Tensor, target: Tensor) -> Tensor:
+        return 2 * (predicted - target)
+
+
+class MSE(Loss):
+    """
+    Mean Squeared Error
+    mse = sum((x - y) ** 2)
+    """
+    def loss(self, predicted: Tensor, target: Tensor) -> float:
+        assert type(predicted) == np.ndarray
+        assert type(target) == np.ndarray
+        mse = np.mean((predicted - target) ** 2)
+        return mse
+
+    def gradient(self, predicted: Tensor, target: Tensor) -> Tensor:
+        gradient = 2 * np.mean(property - target)
+        return gradient
