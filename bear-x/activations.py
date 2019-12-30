b06abb45c80626b5b9bebae53d3374a3ec290963
Bartekkz <bartosz.szmelczynski@gmail.com>
2019-12-30 23:37:42 +0100

    add asserterror messages to all activations

--- bear-x/activations.py
+++ bear-x/activations.py
@@ -9,24 +9,24 @@ import numpy as np
 
 
 def relu(inputs: Tensor) -> Tensor:
-    assert type(inputs) == np.ndarray
+    assert type(inputs) == np.ndarray, "Inputs have to be a Tensor(np.array)!"
     inputs = [np.maximum(0, inputs[node]) for node in range(len(inputs))]
-    return inputs
+    return np.array(inputs)
 
 
 def relu_prime(inputs: Tensor) -> Tensor:
-    assert type(inputs) == np.ndarray
+    assert type(inputs) == np.ndarray, "Inputs have to be a Tensor(np.array)!"
     inputs = [1 if inputs[node] > 0 else 0 for node in range(len(inputs))]
-    return inputs
+    return np.array(inputs)
 
 
 def sigmoid(inputs: Tensor) -> Tensor:
-    assert type(inputs) == np.ndarray
+    assert type(inputs) == np.ndarray, "Inputs have to be a Tensor(np.array)!"
     inputs = [1 / (1 + np.exp(-inputs[node])) for node in range(len(inputs))]
-    return inputs
+    return np.array(inputs)
 
 
 def sigmoid_prime(inputs: Tensor) -> Tensor:
-    assert type(inputs) == np.ndarray
+    assert type(inputs) == np.ndarray, "Inputs have to be a Tensor(np.array)!"
     inputs = [sigmoid(node) * (1 - sigmoid(node)) for node in range(len(inputs))]
-    return inputs
+    return np.array(inputs)
