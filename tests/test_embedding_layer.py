2e8f1de34c293e9599996b813f63b18522d2a219
bartekkz <bartek.szmelczynski@gmail.com>
2020-03-12 21:08:25 +0100

    add new test for embedding layer

--- tests/test_embedding_layer.py
+++ tests/test_embedding_layer.py
@@ -8,5 +8,11 @@ import numpy as np
 
 def test_embedding_layer_output_shape():
     e = Embedding(2, 4)
-    out  = e(np.array([0,1]))
+    out = e(np.array([0,1]))
     assert out.shape == (2, 4)
+
+def test_embedding_layer_with_sequential_model():
+    model = Sequential()
+    model.add(Embedding(3, 6))
+    output = model.feed_forward(np.array([0, 1, 2]))
+    assert output.shape == (3, 6)
