144c4955fb8418a111a2a2952e5c26363b7e539d
bartekkz <bartek.szmelczynski@gmail.com>
2020-03-14 20:18:18 +0100

    add test for flatten layer

---
+++ tests/test_flatten_layer.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("../bearx")
+
+import numpy as np
+
+from layers import Flatten, Embedding, Linear
+from models import Sequential
+
+
+def test_flatten_layer():
+    f = Flatten()
+    inputs = np.array([
+        [1, 2],
+        [3, 4]
+    ])
+    out = f(inputs)
+    print(out)
+
+
+def test_flatten_layer_with_model():
+    model = Sequential()
+    model.add(Embedding(2, 2))
+    model.add(Flatten())
+    model.add(Linear(4, 1))
+
+    inputs = np.array([0, 1])
+    out = model.feed_forward(inputs)
+    print(out)
+
